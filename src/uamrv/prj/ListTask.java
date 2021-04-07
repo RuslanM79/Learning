3ca89a4c97aad5a691a55217038fd958b43cf6b2
RuslanM79 <r.makovetskiy@gmail.com>
2021-04-07 22:14:53 +0300

    Lerning Java These are my home tasks

--- src/uamrv/prj/ListTask.java
+++ src/uamrv/prj/ListTask.java
@@ -1,9 +1,38 @@
 package uamrv.prj;
 /*
-Написать класс содержащий полу типа list содержащий только уникальные элементы (Integer), добавить в нее список не уникальных эелемнтов
+Написать класс типа коллекция, содержащий только уникальные элементы (Integer), добавить в нее список не уникальных эелемнтов
 так, чтобы остались только униклаьные, остортировать по возрастанию и вывести.
  */
 
+import java.util.*;
+
+public class ListTask {
+
+
+    public static void main(String[] args) {
+        Integer[] start = {0, 3, 8, 2, 6, 1, 4, 5, 7, 9, 10};
+        Integer[] toAdd = {10, 10, 1, 2, 33, 33, 45, 65, 99, 99, 108, 108};
+
+        LinkedHashSet<Integer> linkedList = new LinkedHashSet<Integer>(Arrays.asList(start));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+
+        for (int i = 0; i < toAdd.length; i++) {
+            linkedList.add(toAdd[i]);
+        }
+
+        list.addAll(linkedList);
+        Collections.sort(list);
+        linkedList.clear();
+        linkedList.addAll(list);
+        System.out.println(Arrays.asList(linkedList));
+
+
+    }
+
+}
+/*
+Второй вариант
 import java.util.*;
 
 public class ListTask {
@@ -23,3 +52,4 @@ public class ListTask {
     }
 
 }
+ */
